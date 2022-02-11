f55a082e0115f72f772871d08670fb7324796807
Bob Szabo <rszabo50@gmail.com>
2022-02-11 13:45:44 -0500

    version 0.0.9

--- src/ucm/constants.py
+++ src/ucm/constants.py
@@ -23,7 +23,7 @@
 
 PROGRAM_NAME = 'ucm'
 PROGRAM_TITLE = 'Urwid rendered Connection Manager'
-PROGRAM_VERSION = '0.0.8'
+PROGRAM_VERSION = '0.0.9'
 SCM_URL = 'https://github.com/rszabo50/ucm'
 
 MAIN_PALETTE = [
